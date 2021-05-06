4177b09e03b44241386317ceacce9f27b6209107
hissan07 <khairatunhissan@iut-dhaka.edu>
2021-05-06 22:26:42 +0600

    Completed loadContent

--- http-content.js
+++ http-content.js
@@ -3,31 +3,31 @@ const content = require('./loadContent')
 
 const server=http.createServer((req,res)=>{
     if(req.url =="/"){
-        res.write(content.index)
+        res.write(content.content.index)
         res.end()
     }
     if(req.url =="/about"){
-        res.write(content.about)
+        res.write(content.content.about)
         res.end()
     }
     else if(req.url =="/contact"){
-        res.write(content.contact)
+        res.write(content.content.contact)
         res.end()
     }
     else if(req.url =="/blog"){
-        res.write(content.blog)
+        res.write(content.content.blog)
         res.end()
     }
     else if(req.url =="/pricing"){
-        res.write(content.pricing)
+        res.write(content.content.pricing)
         res.end()
     }
     else if(req.url =="/services"){
-        res.write(content.services)
+        res.write(content.content.services)
         res.end()
     }
     else if(req.url =="/work"){
-        res.write(content.work)
+        res.write(content.content.work)
         res.end()
     }
     else {
--- loadContent.js
+++ loadContent.js
@@ -1,68 +1,28 @@
 const fs = require('fs');
 
-const about=fs.readFile("./contents/HTML/about.html","utf-8",(err,data)=>
-{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+const about=fs.readFileSync("./contents/HTML/about.html","utf-8")
 
-const blog=fs.readFile("./contents/HTML/blog.html","utf-8",(err,data)=>
-{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+const blog=fs.readFileSync("./contents/HTML/blog.html","utf-8")
 
-const contact=fs.readFile("./contents/HTML/contact.html","utf-8",(err,data)=>
-{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+const contact=fs.readFileSync("./contents/HTML/contact.html","utf-8")
 
-const index=fs.readFile("./contents/HTML/index.html","utf-8",(err,data)=>
-{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+const index=fs.readFileSync("./contents/HTML/index.html","utf-8")
 
-const pricing=fs.readFile("./contents/HTML/pricing.html","utf-8",(err,data)=>
-{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+const pricing=fs.readFileSync("./contents/HTML/pricing.html","utf-8")
 
-const services=fs.readFile("./contents/HTML/services.html","utf-8",(err,data)=>
-{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+const services=fs.readFileSync("./contents/HTML/services.html","utf-8")
 
-const work=fs.readFile("./contents/HTML/work.html","utf-8",(err,data)=>
-{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+const work=fs.readFileSync("./contents/HTML/work.html","utf-8")
 
-module.exports={
-    about,blog,contact,index,pricing,services,work
+const content = {
+    'about':about,
+    'blog':blog,
+    'contact': contact,
+    'index': index,
+    'pricing': pricing,
+    'services': services,
+    'work': work
 }
+
+
+module.exports={content}
